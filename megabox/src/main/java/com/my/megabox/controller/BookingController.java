a7341108688d6d1d65ac723ef7f87e680b8fe78e
skdysla <skdysla@gmail.com>
2022-08-02 14:20:10 +0900

    copy timetable page

---
+++ megabox/src/main/java/com/my/megabox/controller/BookingController.java
@@ -0,0 +1,14 @@
+package com.my.megabox.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookingController {
+	
+	@GetMapping("timetable")
+	public String timetable() {
+		return "booking/timetable";
+	}
+
+}
